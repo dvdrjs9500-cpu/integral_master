80d4cded2a4e0740e4312777e030eb5e5195e749
dvdrjs9500-cpu <dvdrjs9500@gmail.com>
2026-03-19 19:20:00 +0530

    adding entittles

--- index.ts
+++ index.ts
@@ -1,2 +1,26 @@
-export * from './MemberContext';
-export * from './MemberProvider';
+/**
+ * Auto-generated entity types
+ * Contains all CMS collection interfaces in a single file 
+ */
+
+/**
+ * Collection ID: calculationmethods
+ * Interface for CalculationMethods
+ */
+export interface CalculationMethods {
+  _id: string;
+  _createdDate?: Date;
+  _updatedDate?: Date;
+  /** @wixFieldType text */
+  methodName?: string;
+  /** @wixFieldType text */
+  mathematicalTheory?: string;
+  /** @wixFieldType text */
+  usageInstructions?: string;
+  /** @wixFieldType text */
+  coreFormula?: string;
+  /** @wixFieldType text */
+  exampleProblem?: string;
+  /** @wixFieldType image - Contains image URL, render with <Image> component, NOT as text */
+  methodDiagram?: string;
+}
